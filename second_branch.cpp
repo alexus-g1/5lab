f5c10944269d75d84bc2723310dacfb71f7f5fee
alexus_g1 <antidot2002@yandex.ru>
2023-04-17 16:38:35 +0300

    3rd change in second_branch

--- second_branch.cpp
+++ second_branch.cpp
@@ -4,7 +4,7 @@
 int main() {
     const int size = 10;
     int a[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
+    //3rd change in second_branch.cpp
     double b = 0;
 
     for (int i = 0; i < 10; i++)
