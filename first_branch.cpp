29c76ac16229539a79327505fb0eeee10d1b9c29
alexus_g1 <antidot2002@yandex.ru>
2023-04-17 16:25:19 +0300

    3rd change in first_branch.cpp

--- first_branch.cpp
+++ first_branch.cpp
@@ -2,7 +2,7 @@
 #include <iosream.h>
 #include <math.h>
 void main()
-{
+{//3rd change in first_branch.cpp
 	int a[20] = { 1, 2, 4, 5, 7, 5, 8, 4, 3, 2, 9, 5, 4, 6, 2, 7, 4, 5, 1, 3 };
 	int i, s1, s2;
 	for (i = 0, s1 = 0, s2 = 0; i < 20; ++i)
